6f9cf556ab13e4537909d0bc9cba4db20cdb2a38
Tom Ruff <tomruffcodes@gmail.com>
2024-07-07 14:23:46 -0600

    added the buttons and fixed the color drop downs

--- src/components/wysiwyg/components/ToolBar.jsx
+++ src/components/wysiwyg/components/ToolBar.jsx
@@ -10,13 +10,13 @@ import { ReactComponent as CodeIcon } from "../../../assets/icons/code.svg";
 import { ReactComponent as UnderlineIcon } from "../../../assets/icons/underline.svg";
 import { ReactComponent as StrikeIcon } from "../../../assets/icons/strike.svg";
 import { ReactComponent as QuoteIcon } from "../../../assets/icons/quote.svg";
-import { ReactComponent as JustifyIcon } from "../../../assets/icons/justify.svg";
 import { ReactComponent as HighlightIcon } from "../../../assets/icons/highlight.svg";
 import { ReactComponent as FontcolorIcon } from "../../../assets/icons/fontcolor.svg";
 import { ReactComponent as DividerIcon } from "../../../assets/icons/divider.svg";
 import { ReactComponent as AlignRIcon } from "../../../assets/icons/align-r.svg";
 import { ReactComponent as AlignLIcon } from "../../../assets/icons/align-l.svg";
 import { ReactComponent as AlignCIcon } from "../../../assets/icons/align-c.svg";
+import { ReactComponent as JustifyIcon } from "../../../assets/icons/justify.svg";
 import { ReactComponent as MoreIcon } from "../../../assets/icons/more-v.svg";
 
 const highlightColors = [
@@ -41,8 +41,6 @@ const deepFontColors = [
   "#B12917",
 ];
 
-
-
 const ToolBar = ({
   setCodeModalOpen,
   setLinkModalOpen,
@@ -50,9 +48,8 @@ const ToolBar = ({
   fullContent,
   selectedContent,
 }) => {
-
   // const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
-  const isMac = navigator.userAgent.toLowerCase().includes('macintosh')
+  const isMac = navigator.userAgent.toLowerCase().includes("macintosh");
   const [paragraphDdOpen, setParagraphDdOpen] = useState(false);
   const [highlightDdOpen, setHighlightDdOpen] = useState(false);
   const [colorDdOpen, setColorDdOpen] = useState(false);
@@ -74,92 +71,113 @@ const ToolBar = ({
       setFullContent(formatTxt);
     }
   };
-  const handleOpenClose = (current, setter) => {
-    if (current) {
-      setter(false);
-    } else {
-      setter(true);
-    }
+
+  const handleHighlightClick = (color) => {
+    // e.preventDefault()
+    console.log("clicked color: " + color);
   };
 
+  const handleTxtColorClick = (color) => {
+    // e.preventDefault()
+    console.log("clicked color: " + color);
+  };
 
   const handleClickOutside = (event) => {
-    if (paragraphDropDownRef.current && !paragraphDropDownRef.current.contains(event.target)) {
-      event.stopPropagation()
+    if (
+      paragraphDropDownRef.current &&
+      !paragraphDropDownRef.current.contains(event.target)
+    ) {
+      event.stopPropagation();
       setParagraphDdOpen(false);
     }
-    if (highlightColorDropDownRef.current && !highlightColorDropDownRef.current.contains(event.target)) {
-      event.stopPropagation()
+    if (
+      highlightColorDropDownRef.current &&
+      !highlightColorDropDownRef.current.contains(event.target)
+    ) {
+      event.stopPropagation();
       setHighlightDdOpen(false);
     }
-    if (colorDropDownRef.current && !colorDropDownRef.current.contains(event.target)) {
-      event.stopPropagation()
+    if (
+      colorDropDownRef.current &&
+      !colorDropDownRef.current.contains(event.target)
+    ) {
+      event.stopPropagation();
       setColorDdOpen(false);
     }
   };
-  // if click outside of dropdowns
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-
-  const handleHighlightClick = (e, color) => {
-    e.preventDefault()
-    console.log('clicked color: ' + color)
-  }
-
-  const handleTxtColorClick = (e, color) => {
-    e.preventDefault()
-    console.log('clicked color: ' + color)
-  }
 
   const handleFormatBold = () => applyFormat("b");
   const handleFormatItalic = () => applyFormat("i");
   const handleFormatHeading = (heading) => () => applyFormat(heading);
+
+  // if click outside of dropdowns
+  useEffect(() => {
+    document.addEventListener("mousedown", (e) => handleClickOutside(e));
+    return () => {
+      document.removeEventListener("mousedown", (e) => handleClickOutside(e));
+    };
+  }, []);
 
   return (
     <div className="wysiwyg_tool_bar">
       <div className="icon_button tool_bar" onClick={handleFormatBold}>
         <BoldIcon />
         <span className="wysiwyg_tool_tip">
-          Bold <span className="key_command">{isMac ? "cmd + b" : "ctrl + b"}</span>
+          Bold{" "}
+          <span className="key_command">{isMac ? "cmd + b" : "ctrl + b"}</span>
         </span>
       </div>
       <div className="icon_button tool_bar" onClick={handleFormatItalic}>
         <ItalicIcon />
         <span className="wysiwyg_tool_tip">
-          Italic <span className="key_command">{isMac ? "cmd + i" : "ctrl + i"}</span>
+          Italic{" "}
+          <span className="key_command">{isMac ? "cmd + i" : "ctrl + i"}</span>
         </span>
       </div>
       <div className="icon_button tool_bar">
         <UnderlineIcon />
         <span className="wysiwyg_tool_tip">
-          Underline <span className="key_command">{isMac ? "cmd + u" : "ctrl + u"}</span>
+          Underline{" "}
+          <span className="key_command">{isMac ? "cmd + u" : "ctrl + u"}</span>
         </span>
       </div>
       <div className="icon_button tool_bar">
         <StrikeIcon />
         <span className="wysiwyg_tool_tip">
-          Strike Through <span className="key_command">{isMac ? "cmd + shift + s" : "ctrl + shift + s"}</span>
+          Strike Through{" "}
+          <span className="key_command">
+            {isMac ? "cmd + shift + s" : "ctrl + shift + s"}
+          </span>
         </span>
       </div>
 
       <div className="wysiwyg_tool_bar_divider"></div>
 
-      <div className="icon_button tool_bar tool_bar_dd" ref={colorDropDownRef}>
-        <div className="btn_overlay" onClick={() => setHighlightDdOpen(true)}></div>
+      <div className="icon_button tool_bar tool_bar_dd">
+        <div
+          className="btn_overlay"
+          onClick={() => setHighlightDdOpen(true)}
+        ></div>
         <HighlightIcon />
         <span className="wysiwyg_tool_tip">Highlight Color</span>
-        <div className={`tool_bar_dd_content color_dd ${highlightDdOpen ? "active" : ""}`} >
+        {/* drop down content */}
+        <div
+          className={`tool_bar_dd_content color_dd ${
+            highlightDdOpen ? "active" : ""
+          }`}
+          ref={highlightColorDropDownRef}
+        >
           <div className="highlight_colors">
-            {highlightColors.map(c => (
-              <button onClick={(e) => handleHighlightClick(e, c)} className="color_swatch" key={c} style={{ backgroundColor: c }}></button>
+            {highlightColors.map((c) => (
+              <div
+                key={c}
+                className="color_swatch"
+                onClick={() => handleHighlightClick(c)}
+                style={{ backgroundColor: c }}
+              ></div>
             ))}
           </div>
-          <div className="tool_bar_dd_item p center" >
+          <div className="tool_bar_dd_item p center">
             <span>None</span>
           </div>
         </div>
@@ -169,42 +187,67 @@ const ToolBar = ({
         <div className="btn_overlay" onClick={() => setColorDdOpen(true)}></div>
         <FontcolorIcon />
         <span className="wysiwyg_tool_tip">Font Color</span>
-        <div className={`tool_bar_dd_content color_dd ${colorDdOpen ? "active" : ""}`} ref={highlightColorDropDownRef}>
+        {/* drop down content */}
+        <div
+          className={`tool_bar_dd_content color_dd ${
+            colorDdOpen ? "active" : ""
+          }`}
+          ref={colorDropDownRef}
+        >
           <div className="highlight_colors">
-            {deepFontColors.map(c => (
-              <button onClick={(e) => handleTxtColorClick(e, c)} className="color_swatch" key={c} style={{ backgroundColor: c }}></button>
+            {deepFontColors.map((c) => (
+              <div
+                key={c}
+                className="color_swatch"
+                onClick={() => handleTxtColorClick(c)}
+                style={{ backgroundColor: c }}
+              ></div>
             ))}
           </div>
-          <div className="tool_bar_dd_item p center" >
+          <div className="tool_bar_dd_item p center">
             <span>Automatic</span>
           </div>
         </div>
       </div>
 
       <div className="icon_button tool_bar tool_bar_dd">
-        <p onClick={() => setParagraphDdOpen(true)}>
-          Paragraph
-        </p>
+        <p onClick={() => setParagraphDdOpen(true)}>Paragraph</p>
         <div
-          className={`tool_bar_dd_content ${paragraphDdOpen ? "active" : ""}`} ref={paragraphDropDownRef}
+          className={`tool_bar_dd_content ${paragraphDdOpen ? "active" : ""}`}
+          ref={paragraphDropDownRef}
         >
-          <div className="tool_bar_dd_item h1" onClick={handleFormatHeading("h1")}>
+          <div
+            className="tool_bar_dd_item h1"
+            onClick={handleFormatHeading("h1")}
+          >
             <span>Heading 1</span>
             <H1Icon />
           </div>
-          <div className="tool_bar_dd_item h2" onClick={handleFormatHeading("h2")}>
+          <div
+            className="tool_bar_dd_item h2"
+            onClick={handleFormatHeading("h2")}
+          >
             <span>Heading 2</span>
             <H2Icon />
           </div>
-          <div className="tool_bar_dd_item h3" onClick={handleFormatHeading("h3")}>
+          <div
+            className="tool_bar_dd_item h3"
+            onClick={handleFormatHeading("h3")}
+          >
             <span>Heading 3</span>
             <H3Icon />
           </div>
-          <div className="tool_bar_dd_item h4" onClick={handleFormatHeading("h4")}>
+          <div
+            className="tool_bar_dd_item h4"
+            onClick={handleFormatHeading("h4")}
+          >
             <span>Heading 4</span>
             <H4Icon />
           </div>
-          <div className="tool_bar_dd_item p" onClick={handleFormatHeading("p")}>
+          <div
+            className="tool_bar_dd_item p"
+            onClick={handleFormatHeading("p")}
+          >
             <span>Paragraph</span>P
           </div>
         </div>
@@ -222,6 +265,49 @@ const ToolBar = ({
       >
         <CodeIcon />
         <span className="wysiwyg_tool_tip">Code Block</span>
+      </div>
+
+      <div
+        className="icon_button tool_bar heading"
+      >
+        <QuoteIcon />
+        <span className="wysiwyg_tool_tip">Quote</span>
+      </div>
+      <div
+        className="icon_button tool_bar heading"
+      >
+        <DividerIcon />
+        <span className="wysiwyg_tool_tip">Divider</span>
+      </div>
+      <div
+        className="icon_button tool_bar heading"
+      >
+        <AlignRIcon />
+        <span className="wysiwyg_tool_tip">Align Right</span>
+      </div>
+      <div
+        className="icon_button tool_bar heading"
+      >
+        <AlignLIcon />
+        <span className="wysiwyg_tool_tip">Align Left</span>
+      </div>
+      <div
+        className="icon_button tool_bar heading"
+      >
+        <AlignCIcon />
+        <span className="wysiwyg_tool_tip">Align Center</span>
+      </div>
+      <div
+        className="icon_button tool_bar heading"
+      >
+        <JustifyIcon />
+        <span className="wysiwyg_tool_tip">Justify Text</span>
+      </div>
+      <div
+        className="icon_button tool_bar heading"
+      >
+        <MoreIcon />
+        <span className="wysiwyg_tool_tip">More</span>
       </div>
     </div>
   );

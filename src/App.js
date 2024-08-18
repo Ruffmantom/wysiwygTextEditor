eb5b5cd24cf184c9f3dac33c4251fb38aca565fb
Tom Ruff <tomruffcodes@gmail.com>
2024-08-17 22:07:04 -0600

    fixed merge changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import EditorJs from "./components/editorjs/RichTextEditor";
+// import EditorJs from "./components/editorjs/RichTextEditor";
 import MyEditor from "./components/MyEditor/myEditor";
-import RichTextEditor from "./components/richTextEditor/RichTextEditor";
+// import RichTextEditor from "./components/richTextEditor/RichTextEditor";
 
 function App() {
   return (
     <div className="App">
       <div className="content">
-<<<<<<< HEAD
         {/* <EditorJs /> */}
         {/* Examples */}
         <MyEditor />
-=======
->>>>>>> 271fac3fad486a15c00d9759f4ea0d7a20258212
         {/* WIP Tool */}
         {/* <RichTextEditor
           options={{
@@ -35,23 +32,8 @@ function App() {
               info: true,
               monospace: true,
             },
-<<<<<<< HEAD
           }}
         /> */}
-=======
-            link: true,
-            code: true,
-            quote: true,
-            divider: true,
-            orderedList: true,
-            unorderedList: true,
-            info: true,
-            monospace: true,
-          }
-        }} />
-
-
->>>>>>> 271fac3fad486a15c00d9759f4ea0d7a20258212
       </div>
     </div>
   );
